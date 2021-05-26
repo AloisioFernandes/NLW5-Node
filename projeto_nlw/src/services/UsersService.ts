1bc04a41e472251c0443a2eceb7f0c2174a089a6
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-25 23:02:21 -0300

    Rota, service e controller de usuários.

---
+++ projeto_nlw/src/services/UsersService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from "typeorm"
+
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+  async create(email: string) {
+    const usersRepository = getCustomRepository(UsersRepository)
+    // Verificar se usuário existe
+
+    const userExists = await usersRepository.findOne({
+      email
+    })
+
+    // Se existir, retornar user
+    if(userExists) {
+      return userExists
+    }
+
+    // Se não existir, criar, salvar no BD e retorná-lo
+    const user = usersRepository.create({
+      email
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+}
+
+export { UsersService }
