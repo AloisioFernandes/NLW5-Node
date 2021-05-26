1517817e866aa921c45970436a6b56117c9af416
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-25 22:36:11 -0300

    Migration, entidade e repositório de usuários.

--- projeto_nlw/src/repositories/SettingsRepository.ts
+++ projeto_nlw/src/repositories/SettingsRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from "typeorm";
 
 import { Setting } from "../entities/Setting";
 
-@EntityRepository(Setting)
+@EntityRepository(Setting) // faz todas as manipulações de entidade no banco de dados
 class SettingsRepository extends Repository<Setting> {}
 
 export { SettingsRepository }
