1d53a0d1184dc9f0d465a4a7c1a66cfc0aad1a6f
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-05-31 22:32:43 -0300

    Migration das conexões.

--- projeto_nlw/src/websocket/client.ts
+++ projeto_nlw/src/websocket/client.ts
@@ -3,5 +3,7 @@ import { io } from '../http'
 io.on('connect', (socket) => {
   socket.on('client_first_access', params => {
     console.log(params)
+
+    // Salvar a conexão com o socket_id, user_id
   })
 })
