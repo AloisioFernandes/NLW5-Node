b8c0ef2404906dd80e592fe2d34c0e7d0943b20d
AloisioFernandes <aloisio.fernandes@outlook.com>
2021-06-03 22:04:58 -0300

    Exibindo mensagens anteriores do usuário.

--- projeto_nlw/src/websocket/client.ts
+++ projeto_nlw/src/websocket/client.ts
@@ -50,6 +50,8 @@ io.on('connect', (socket) => {
       user_id
     })
 
-    // Salvar a conexão com o socket_id, user_id
+    const allMessages = await messagesService.listByUser(user_id)
+
+    socket.emit("client_list_all_messages", allMessages)
   })
 })
